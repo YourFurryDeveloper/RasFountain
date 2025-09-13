aa928ac5dea69fb69dbb589289dc4dd4842e6951
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-09-13 16:28:39 -0400

    servo

--- drinkMixer.py
+++ drinkMixer.py
@@ -5,13 +5,13 @@ pi = pigpio.pi()
 pi.set_mode(12, pigpio.OUTPUT)
 
 def openValve():
-    pi.set_servo_pulsewidth(12, 500)
+    pi.hardware_PWM(12, 50, 3000)
     
 def closeValve():
-    pi.set_servo_pulsewidth(12, 1300)
+    pi.hardware_PWM(12, 50, 30589)
     
 def init():
-    pi.set_PWM_frequency(12, 500)
+    pi.set_PWM_frequency(12, 50)
     closeValve()
 
 def mix(drinkName):
