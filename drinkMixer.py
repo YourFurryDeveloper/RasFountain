fe9f17262fc4c1b6ef6c7a648a8b37629bbb613f
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-09-13 16:33:45 -0400

    servo update

--- drinkMixer.py
+++ drinkMixer.py
@@ -5,11 +5,15 @@ pi = pigpio.pi()
 pi.set_mode(12, pigpio.OUTPUT)
 
 def openValve():
-    pi.hardware_PWM(12, 50, 3000)
-    
+    pi.hardware_PWM(12, 50, 3000)  # Move servo
+    time.sleep(0.5)                # Let it reach the position
+    pi.hardware_PWM(12, 0, 0)      # Stop PWM to prevent shudder
+
 def closeValve():
     pi.hardware_PWM(12, 50, 30589)
-    
+    time.sleep(0.5)
+    pi.hardware_PWM(12, 0, 0)
+
 def init():
     pi.set_PWM_frequency(12, 50)
     closeValve()
@@ -19,7 +23,7 @@ def mix(drinkName):
         openValve()
         time.sleep(1)
         closeValve()
-        
+
 def stopPumpD():
     pi.stop()
     print("Pigpio daemon stopped")
