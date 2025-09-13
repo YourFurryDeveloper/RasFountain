bed9b46c300c7ae9aa58633b8f33873859d79ef0
YourFurryDeveloper <thatonefurryreal@gmail.com>
2025-09-13 13:17:42 -0400

    Hopefully working :3

--- drinkMixer.py
+++ drinkMixer.py
@@ -1,3 +1,13 @@
+import rpi.gpio as gpio
+import time
+
+gpio.setmode(gpio.BCM)
+gpio.setup(23, gpio.OUT)
+
 def mix(drinkName):
     if drinkName == "Water":
-        print("Water :3")
+        gpio.output(23, gpio.HIGH)
+        time.sleep(1)
+        gpio.output(23, gpio.LOW)
+        
+        gpio.cleanup()
